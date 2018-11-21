ab2c99841b90763278f8ca26b95e7764a37369a3
fguardiola <fernando.guardiola.888@gmail.com>
2018-11-21 11:13:37 +0100

    Add dataSet structs & basic home UI

---
+++ GoodEatings/Model/FoodCategory.swift
@@ -0,0 +1,15 @@
+//
+//  FoodCategory.swift
+//  GoodEatings
+//
+//  Created by 67621177 on 21/11/2018.
+//  Copyright © 2018 67621177. All rights reserved.
+//
+
+import Foundation
+
+
+struct FoodCategory {
+    let title : String
+    let imageName: String
+}
---
+++ GoodEatings/Model/Recipe.swift
@@ -0,0 +1,16 @@
+//
+//  Recipe.swift
+//  GoodEatings
+//
+//  Created by 67621177 on 21/11/2018.
+//  Copyright © 2018 67621177. All rights reserved.
+//
+
+import Foundation
+
+
+struct Recipe {
+    let title: String
+    let instructions: String
+    let imageName: String
+}
